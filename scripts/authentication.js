28cac07da9e9f12ccd840a76e76593249443cdcd
ALEXIE NARCISO <NA.LEXIE.AN@gmail.com>
2022-03-09 14:23:24 -0800

    tweaked authentication id to link to html

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -30,7 +30,7 @@ var uiConfig = {
       uiShown: function() {
         // The widget is rendered.
         // Hide the loader.
-        document.getElementById('login-loader').style.display = 'none';
+        document.getElementById("loader").style.display = 'none';
       }
     },
     // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
