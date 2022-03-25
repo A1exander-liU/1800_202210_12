82b7bd400dcf2f4e712576519bf8568520966864
Toco Tachibana <ttachibana@my.bcit.ca>
2022-03-25 13:50:02 -0700

    Rework scan.js

--- scripts/scan.js
+++ scripts/scan.js
@@ -1,58 +1,47 @@
-function createCardData() {
-    max = 21;
-    //define a variable for the collection you want to create in Firestore to populate data
-    var hikesRef = db.collection("My-Tickets");
-    for (i = 1; i <= max; i++) {
-        hikesRef.add({ //add to database, autogen ID
-            confirmation: "id" + i,
-            event: "event" + i,
-            name: "user" + i,
-            email: "email" + i,
-            quantity: "quantity" + i,
-        })
+// timestamp
+$(".pocket").click(function() {
+  const timestamp = firebase.firestore.Timestamp.now();
+  console.log(timestamp);
 
-        // createCardData();
-   }
+  firebase.auth().onAuthStateChanged(user => {
+    if (user) {
+      var currentUser = db.collection("users").doc(user.uid);
+      currentUser.get().then(userDoc => {
+        var userName = userDoc.data().name;
+        console.log(userName);
+      })
+
+      db.collection("history").add({
+        name: currentUser,
+        timeStamp: timestamp,
+      })
+    }
+  })
+})
+
+
+// Populate history cards
+function displayHistoryCards(collection) {
+  let eventTemplate = document.getElementById("historyTemplate");
+
+  db.collection(collection).get()
+    .then(snap => {
+      var i = 1;
+      snap.forEach(doc => { //iterate thru each doc
+        var timestamp = doc.data().timeStamp;
+        var date = timestamp.toDate();
+        console.log(date);
+        let newcard = historyTemplate.content.cloneNode(true);
+
+        newcard.querySelector('strong').innerHTML = i + " :"
+        newcard.querySelector('.time-stamp').innerHTML = date;
+
+        document.getElementById("historyList").appendChild(newcard);
+        i++;
+      })
+    })
 }
 
-
-function displayCards(collection) {
-    let cardTemplate = document.getElementById("eventcardtemplate");
-
-    db.collection(collection).get()
-        .then(snap => {
-            var i = 1;
-            snap.forEach(doc => { //iterate thru each doc
-                var title = doc.data().name;   // get value of the "name" key
-                var details = doc.data().details;   // get value of the "details" key
-                let newcard = cardTemplate.content.cloneNode(true);
-
-                //update title and text and image
-                newcard.querySelector('.event-title').innerHTML = title;
-                newcard.querySelector('.card-text').innerHTML = details;
-                newcard.querySelector('.card-image').src = "./images/" + collection + ".jpg"; //hikes.jpg
-
-
-                document.getElementById(collection + "events-go-here").appendChild(newcard);
-                i++;
-            })
-        })
-}
-
-// displayCards("scanables");
-
-
-function pastScans() {
-  var scanRef = db.collection("scans");
-
-  scanRef.add({
-    scanID: "000000000001",
-  });
-}
-
-
-function recordScan() {
-  db.collection("scans").add({
-    date: firebase.firestore.Timestamp.fromDate(new Date("March 11, 2022"))
-  });
+if($("body").is("#historyPage")){
+  displayHistoryCards("history");
 }
