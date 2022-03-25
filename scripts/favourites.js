df1c277bd5bbee16e9b7d1e3f2a7aa858287660e
ALEXIE NARCISO <NA.LEXIE.AN@gmail.com>
2022-03-25 10:50:38 -0700

    ass var currentUser

--- scripts/favourites.js
+++ scripts/favourites.js
@@ -1,26 +1,37 @@
-function getFavourites(currentUser) {
+firebase.auth().onAuthStateChanged(user => {
+    if (user){
+        getfavourites(user)
+    } else {
+        console.log("NO user on account")
+    }
+})
+
+
+function getFavourites(user) {
     db.collection("users").doc(user.uid).get()
         .then(userDoc => {
             var favourites = userDoc.data().favourites;
             console.log(favourites);
 
             let CardTemplate = document.getElementById("eventCardTemplate");
-            favourites.forEach(thiseventID => {
-                console.log(thiseventID);
-                db.collection("events").where("id", "==", thiseventID).get().then(snap => {
+            favourites.forEach(thiseventTitle => {
+                console.log(thiseventTitle);
+                db.collection("events").where("event_title", "==", thiseventID).get().then(snap => {
                     size = snap.size;
                     queryData = snap.docs;
                     
                     if (size == 1) {
                         var doc = queryData[0].data();
-                        var EventTitle = doc.title; //gets the name field
-                        var eventID = doc.id; //gets the unique ID field
+                        var eventTitle = doc.envent_title; //gets the name field 
+                        var eventInfo = doc.info;
+                        var eventID = doc.id //gets the unique ID field
                         let newCard = CardTemplate.content.cloneNode(true);
-                        newCard.querySelector('.card-title').innerHTML = eventName;
-                        newCard.querySelector('a').onclick = () => setEventData(eventID);
-                        hikeCardGroup.appendChild(newCard);
+                        newCard.querySelector('.card-title').innerHTML = eventTitle;
+                        newCard.querySelector('.card-info').innerHTML = eventInfo;
+                        newCard.querySelector('a').onclick = () => setEventData(eventID); //NO EVENT ID?!?
+                        eventCardGroup.appendChild(newCard);
                     } else {
-                        console.log("thes a number of query data")
+                        console.log("lots of query data")
                     }
 
                 })
