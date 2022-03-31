6c058187de362d23f48a4f74e453bb8a84b65f4a
A1exander-liU <alexanderliu2016@gmail.com>
2022-03-30 21:14:10 -0700

    added restriction to profile, favourites to guests

--- scripts/signout.js
+++ scripts/signout.js
@@ -1,17 +1,21 @@
+// this piece of code will basically change the button functionality depending if user is signed in or not
 firebase.auth().onAuthStateChanged(user => {
     // Check if user is signed in:
-    if (user) {
-        $('#login').attr('href', '../index.html')
-        $('#login').text('Sign Out')
+    if (user) { // if they are
+        $('#login').attr('href', '../index.html') // change the linked html page to index.html
+        $('#login').text('Sign Out') // change the button text to signout 
     }
-    else {
-        $('#login').attr('href', '../login.html')
-        $('#login').text('Login')
+    else { // if they are not
+        $('#login').attr('href', '../login.html') // change the linked html page to login.html
+        $('#login').text('Login') // change the button text to login
+
+        $('#profile').attr('href', '#') // changing the link to blank so they don't get redirected
+        $('#favourites').attr('href', '#') // changed the link to blank so they don't get redirected
     }
 })
 
 function signout(){
-    firebase.auth().signOut().then(() => {
+    firebase.auth().signOut().then(() => { // signing out the user
         console.log("Successful signup")
       }).catch((error) => {
         console.log(error)
@@ -19,9 +23,20 @@ function signout(){
       
 }
 
+function show_login_prompt(){
+    if ($(this).attr('id') == 'profile') {
+        $("#restricted").text("You cannot view your history of scans if you have not signed in.")
+        $("#profile_prompt").modal('show')
+    }
+    if ($(this).attr('id') == 'favourites') {
+        $("#restricted").text("You cannot view your favourited events if you have not signed in.")
+        $("#profile_prompt").modal('show')
+    }
+}
 
 function setup(){
-    $('body').on('click', '#login', signout)
+    $('body').on('click', '#login', signout) // when you click on the sign in/sign out button call signout function
+    $('body').on('click', 'a', show_login_prompt)
 }
 
 $(document).ready(setup)
