dbeb67b9263671b1b1566bfdb16cb990ccd5b9e4
A1exander-liU <alexanderliu2016@gmail.com>
2022-03-30 21:44:16 -0700

    added prompt modal to map and events

--- scripts/signout.js
+++ scripts/signout.js
@@ -2,7 +2,7 @@
 firebase.auth().onAuthStateChanged(user => {
     // Check if user is signed in:
     if (user) { // if they are
-        $('#login').attr('href', '../index.html') // change the linked html page to index.html
+        $('#login').attr('href', '../main.html') // change the linked html page to index.html
         $('#login').text('Sign Out') // change the button text to signout 
     }
     else { // if they are not
@@ -12,6 +12,7 @@ firebase.auth().onAuthStateChanged(user => {
         $('#profile').attr('href', '#') // changing the link to blank so they don't get redirected
         $('#favourites').attr('href', '#') // changed the link to blank so they don't get redirected
         $('#wallet').attr('href', '#')
+        $('#history').attr('href', '#')
     }
 })
 
@@ -26,16 +27,20 @@ function signout(){
 
 function show_login_prompt(){
     if ($(this).attr('id') == 'profile') { // checking if the id of the link you clicked on is id of link to profile.html
-        $("#restricted").text("You cannot view your history of scans if you have not signed in.") // customizing the message
-        $("#profile_prompt").modal('show') // diplaying the modal
+        $(".restricted").text("You cannot view your profile if you have not signed in.") // customizing the message
+        $(".profile_prompt").modal('show') // diplaying the modal
     }
     if ($(this).attr('id') == 'favourites') { // checking if the id of the link you click is id of link to favourites.html
-        $("#restricted").text("You cannot view your favourited events if you have not signed in.") // customizing the message
-        $("#profile_prompt").modal('show')
+        $(".restricted").text("You cannot view your favourited events if you have not signed in.") // customizing the message
+        $(".profile_prompt").modal('show')
     }
     if ($(this).attr('id') == 'wallet') { // checking if the id of the link you click is id of link to wallet.html
-        $("#restricted").text("You cannot view your wallet if you have not signed in.") // customizing the message
-        $("#profile_prompt").modal('show')
+        $(".restricted").text("You cannot view your wallet if you have not signed in.") // customizing the message
+        $(".profile_prompt").modal('show')
+    }
+    if ($(this).attr('id') == 'history') { // checking if the id of the link you click is id of link to wallet.html
+        $(".restricted").text("You cannot view your history of scans if you have not signed in.") // customizing the message
+        $(".profile_prompt").modal('show')
     }
 }
 
