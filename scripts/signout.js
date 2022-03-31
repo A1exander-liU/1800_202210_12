6737e22ad082b0fb1b0d56dd7b329c40fd3af0fe
A1exander-liU <alexanderliu2016@gmail.com>
2022-03-30 20:29:42 -0700

    added signout

---
+++ scripts/signout.js
@@ -0,0 +1,27 @@
+firebase.auth().onAuthStateChanged(user => {
+    // Check if user is signed in:
+    if (user) {
+        $('#login').attr('href', '../index.html')
+        $('#login').text('Sign Out')
+    }
+    else {
+        $('#login').attr('href', '../login.html')
+        $('#login').text('Login')
+    }
+})
+
+function signout(){
+    firebase.auth().signOut().then(() => {
+        console.log("Successful signup")
+      }).catch((error) => {
+        console.log(error)
+      });
+      
+}
+
+
+function setup(){
+    $('body').on('click', '#login', signout)
+}
+
+$(document).ready(setup)
