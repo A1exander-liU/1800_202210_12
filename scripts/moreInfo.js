3dd3d37ef3f70cc6cb61b8028d601a69c9b84139
ALEXIE NARCISO <NA.LEXIE.AN@gmail.com>
2022-03-31 18:25:30 -0700

    placing tag in divs for more info

--- scripts/moreInfo.js
+++ scripts/moreInfo.js
@@ -1,13 +1,21 @@
 /// this a function that creates a URL of the selected object then display the remaining details left out from the cards thats in the database///
-function showDetails(){
+function showDetails(eventID) {
+   
+    console.log( eventID + "is inside the show details")
     let params = new URL(window.location.href); ///ceates a URL object
-    let id = params.searchParams.get("event-title");  //parses the event infos from collection 
-    let type = params.searchParams.get("card-type"); 
-    let genre = params.searchParams.get("card-genre");
-    let location = params.searchParams.get("card-location");  
-    let details = params.searchParams.get("event-info");
-    let date = params.searchParams.get("card-date");  
-    let time = params.searchParams.get("card-time"); 
-} 
+    let name = params.searchParams.get("name")
+    console.log("MY name:" + name)
+    let image = params.searchParams.get("image")
+    let details = params.searchParams.get("details")
+    
+    // newCard.querySelector('name').innerHTML = id ;
+    // newCard.querySelector('image') = image ;
+    // newCard.querySelector('details') = details ;
+    document.getElementsByClassName("name").innerHTML = name;  
+    document.getElementsByClassName("details").innerHTML = details; 
+    // $(".name").innerHTML(id);
+    
+   
+}
 
-showDetails()
+showDetails();
