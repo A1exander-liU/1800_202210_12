d5169f1d1bfe177d0e5cc69d9c79f22134ffd42b
ALEXIE NARCISO <NA.LEXIE.AN@gmail.com>
2022-04-03 20:49:28 -0700

    populate title and details to moreInfo

--- scripts/moreInfo.js
+++ scripts/moreInfo.js
@@ -1,21 +1,40 @@
 /// this a function that creates a URL of the selected object then display the remaining details left out from the cards thats in the database///
-function showDetails() {
 
+function showDetails() {
+    var details = ''
     let params = new URL(window.location.href); ///creates a URL object
     var name = params.searchParams.get("eventName")
     console.log("MY name:" + name + "myparams: " + params)
 
     db.collection("events").where('name', '==', name)
         .get()
-        .then((querySnapshot) => {
-            querySnapshot.forEach((doc) => {
-                console.log(`${doc.id} => ${doc.data()}`);
-                var details = doc.data(details)
-                console.log("MY name:" + name + " details" + details)
+        .then(function (querySnapshot) {
+            querySnapshot.forEach(function (doc) {
+                console.log(doc.id, " => ", doc.data());
+                var data = doc.data();
+                details = data.details
+
+                console.log("these are inside: " + details);
+
+                document.querySelector(".details").innerHTML = details;
+
+
+
             });
+
         });
 
-    document.getElementsByClassName("name").innerHTML=name;
+
+
+
+
+
+    document.querySelector(".name").innerHTML = name;
+    console.log("these are outside: " + name + details)
+
+
+
+
 }
 
 showDetails();
